13b7b787c7a0fa11a6e63b37d8fe8ee54e815298
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-06-01 03:00:50 -0700

    Editing code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(str):
         if letter_code >= 97 and letter_code <= 122:
             letter_code = letter_code - 32
         print("{}".format(chr(letter_code)), end="")
-    print()        
+    print()
