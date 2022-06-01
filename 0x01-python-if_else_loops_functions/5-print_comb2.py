de2c98545e84572f6a8424b782f52ec4b206bec4
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-06-01 01:43:42 -0700

    Added file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num != 99:
-        print(f"{num},", end = " ")
+        print("{},".format(num), end = " ")
     else:
-        print(f"{num}")
+        print("{}".format(num))
