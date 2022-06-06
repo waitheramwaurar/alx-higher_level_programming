6689b548d0f2419738466f797caa5ee0676c5432
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-06-06 13:52:43 -0700

    Removing max function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,4 +3,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return "None"
     else:
-        return max(my_list)
+        highest = 0
+        for i in my_list:
+            if i > highest:
+                highest = i
+        return highest
