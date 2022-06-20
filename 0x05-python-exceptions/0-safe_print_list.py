c9dc3316018a9defe0db7904a497bfccdca0e29e
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-06-20 14:09:55 -0700

    Added files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print("{}".format(my_list[x]), end='')
+            print("{}".format(my_list[i]), end='')
             item += 1
         except IndexError:
             pass
